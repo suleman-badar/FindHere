a6fef281a14d3e0ace8fdddfbedff3e2b360d4be
Ali Maqsood <alimaqsood35892@gmail.com>
2025-08-17 17:25:50 +0500

    Added User model and pre hashed the password

--- backend/index.js
+++ backend/index.js
@@ -7,9 +7,6 @@ const app = express();
 
 const PORT = process.env.PORT || 8000;
 
-app.get("/", (req, res) => {
-    res.send("Server running!");
-});
 
 app.listen(PORT, () => {
     console.log(`Server started on http://localhost:${PORT}`);
