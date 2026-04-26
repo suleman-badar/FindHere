734fcd328505f3ae419c703c9a8a008befb1d888
Suleman Badar <suleman.badar.butt@gmail.com>
2026-04-26 12:31:53 +0500

    added Vite localhost URl in .env file

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const DEFAULT_BASE = "http://localhost:7700";
+const DEFAULT_BASE =  import.meta.VITE_API_BASE_URL;
 const rawBase = import.meta.env.VITE_API_BASE_URL || DEFAULT_BASE;
 const API_BASE = String(rawBase).replace(/\/+$/, "");
 
